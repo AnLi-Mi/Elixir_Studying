00c8220dcdb0eb8eee3bc49a1b33331d690ff167
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-13 19:38:53 +0200

    toying with lists and their functions

--- first_tutorial.ex
+++ first_tutorial.ex
@@ -128,7 +128,33 @@ IO.puts(String.split(my_string)) # works more like 'join' tan 'split'...
 IO.puts(String.upcase(my_string))
 
 
+# testing lists
 
-# 3 -
+# 1 - concatenating and subtracting
+x = ["fox", "rabbit", "chicken"]
+y = ["rooster", "duck", "chicken"]
+IO.puts(x ++ y)
+IO.puts(y -- x)
 
-# 4 -
+IO.puts(["fox", "rabbit", "chicken"] ++ ["rooster", "chicken"]) # why is it displayed as a string, no whitespaces, no comas?
+IO.puts(["rooster", "chicken"] -- ["fox", "rabbit", "chicken"])
+
+# 2 - heads and tails
+
+IO.puts(hd(x))
+IO.puts(tl(y))
+
+list = [1, 'parrot', 'mouse', 'moose']
+IO.puts(hd(list))
+IO.puts(tl(list)) # it shows all lists elements apart form the first element
+
+# 3 - lists functions
+
+my_list = ["rooster", "duck", "chicken", "dog", "cat"]
+
+IO.puts(my_list)
+IO.puts(List.delete(my_list, "duck"))
+IO.puts(List.delete_at(my_list, 2)) # usuwa kolejny po wskazanym elemencie
+IO.puts(List.first(my_list))
+IO.puts(List.insert_at(my_list, 4, "extra value")) # dodaje kolejny po wskazanym elemencie
+IO.puts(List.last(my_list))
