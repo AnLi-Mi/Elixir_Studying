57aef5da537843a144bd01be2db8cbb0efdeebae
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-13 16:52:15 +0200

    testing outputs, line endings, variables, operators

--- first_tutorial.ex
+++ first_tutorial.ex
@@ -15,3 +15,41 @@ IO.puts("World!")
 
 #4
 # not working - IO.puts("Hello") IO.puts("World!")
+
+# testing variables
+
+life =8
+IO.puts life
+IO.puts(life)
+
+life = "hello"
+IO.puts life
+IO.puts(life)
+
+# testing operators
+
+#1
+a=10
+b=20
+
+IO.puts (a+b)
+IO.puts (a-b)
+IO.puts (a/b)
+IO.puts (a*b)
+IO.puts (div(b,a))
+IO.puts (rem(b,a))
+IO.puts (a==b)
+IO.puts (a===b)
+
+#2
+a=5
+b="hello"
+
+# nia da sie - IO.puts (a+b)
+# nia da sie - IO.puts (a-b)
+# nia da sie - IO.puts (a/b)
+# nia da sie - IO.puts (a*b)
+# nia da sie - IO.puts (div(b,a))
+# nia da sie - IO.puts (rem(b,a))
+IO.puts (a==b)
+IO.puts (a===b)
