720b2155d309176b8e9e151b22f06e378bd0d1ad
AnLi-Mi <a.lipiarz@gmail.com>
2020-10-13 16:08:15 +0200

    setting up environment

---
+++ first_tutorial.ex
@@ -0,0 +1,17 @@
+IO.puts "hello world"
+
+#testing different line endings
+
+#1
+IO.puts("Hello"); IO.puts("World!")
+
+#2
+IO.puts("Hello")
+IO.puts("World!")
+
+#3
+IO.puts("Hello");
+IO.puts("World!")
+
+#4
+# not working - IO.puts("Hello") IO.puts("World!")
